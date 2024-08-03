288a76ab918678d8e7e315e104ba71580eb384f5
Mahiedine9 <mahieddine.ferdjoukh9@gmail.com>
2024-08-03 23:16:39 +0200

    isdaytime

--- src/view/WeatherCard.tsx
+++ src/view/WeatherCard.tsx
@@ -7,15 +7,17 @@ interface WeatherCardProps {
   location: string;
   temperature: number;
   weatherCondition: string;
+  isDaytime: boolean;
   forecast: { date: string; condition: string; temp: number }[];
 }
 
-const WeatherCard: React.FC<WeatherCardProps> = ({ heading, location, temperature,weatherCondition, forecast }) => {
+const WeatherCard: React.FC<WeatherCardProps> = ({ heading, location, temperature, weatherCondition, isDaytime, forecast }) => {
 
   const weatherClass = weatherCondition.toLowerCase();
+  const timeClass = isDaytime ? 'daytime' : 'nighttime';
 
   return (
-    <div className={`weather-card ${weatherClass}`}>
+    <div className={`weather-card ${weatherClass} ${timeClass}`}>
       <div className="top">
         <div className="wrapper">
           <div className="mynav">
@@ -25,7 +27,7 @@ const WeatherCard: React.FC<WeatherCardProps> = ({ heading, location, temperatur
           <h3 className="location">{location}</h3>
           <p className="temp">
             <span className="temp-value">{temperature}</span>
-            <span className="deg">0</span>
+            <span className="deg">°</span>
             <a href="#"><span className="temp-type">C</span></a>
           </p>
         </div>
@@ -38,7 +40,7 @@ const WeatherCard: React.FC<WeatherCardProps> = ({ heading, location, temperatur
               <li key={index} className={index === 0 ? 'active' : ''}>
                 <span className="date">{day.date}</span>
                 <span className={`lnr lnr-${day.condition.toLowerCase()} condition`}>
-                  <span className="temp">{day.temp}<span className="deg">0</span><span className="temp-type">C</span></span>
+                  <span className="temp">{day.temp}<span className="deg">°</span><span className="temp-type">C</span></span>
                 </span>
               </li>
             ))}
